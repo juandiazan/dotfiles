9fe86f13c01cce352052b319b0da3b8970c73e97
Juan Diaz <juanignaciodiazpuig998@gmail.com>
2025-10-16 23:45:50 -0300

    refactor: extracted dir creation fuction

--- create-dotfiles-backup.sh
+++ create-dotfiles-backup.sh
@@ -1,27 +1,43 @@
 #!/bin/zsh
 
-currentDate=`date +%d-%m-%Y_%H:%M:%S`
-
-braveBrowserBookmarks="$HOME/.config/BraveSoftware/Brave-Browser/Default/Bookmarks"
-zshConfig="$HOME/.zshrc"
-
-dotfilesBackupDir="$HOME/dotfiles"
-braveBackupDirectory="$dotfilesBackupDir/brave-browser"
-zshBackupDirectory="$dotfilesBackupDir/zsh-and-omz-config"
-
+# utilities
 BOLD_RED='\033[1;31m'
 BOLD_GREEN='\033[1;32m'
 BOLD_YELLOW='\033[1;33m'
+BOLD_PURPLE='\033[1;35m'
 NO_COLOR='\033[0m'
+currentDate=`date +%d-%m-%Y_%H:%M:%S`
+
+# dotfiles locations
+braveBrowserBookmarks="$HOME/.config/BraveSoftware/Brave-Browser/Default/Bookmarks"
+zshConfig="$HOME/.zshrc"
+
+# dotfiles destinations
+dotfilesBackupDir="$HOME/dotfiles"
+braveBackupDirectory="$dotfilesBackupDir/brave-browser"
+zshBackupDirectory="$dotfilesBackupDir/zsh-and-omz-config"
 
 print_color()
 {
     echo -e $1$2$NO_COLOR
 }
 
-echo "-----------------------------"
-echo "------ dotfile backup -------"
-echo "-----------------------------"
+create_dir_if_not_exists()
+{
+    if [ ! -d $1 ]; then
+        print_color $BOLD_YELLOW "Directory $1 not found. Creating..."
+        mkdir -p $1 
+        print_color $BOLD_GREEN "Created $1"
+    fi
+}
+
+print_color $BOLD_PURPLE "-----------------------------"
+print_color $BOLD_PURPLE "------ dotfile backup -------"
+print_color $BOLD_PURPLE "-----------------------------"
+
+create_dir_if_not_exists $braveBackupDirectory
+create_dir_if_not_exists $zshBackupDirectory
+
 echo "Backing up files..."
 
 # -------------------------------------------------------
@@ -33,23 +49,11 @@ if [ -f $braveBrowserBookmarks ]; then
     echo "."
     echo "."
     echo "."
-    if [ -d $braveBackupDirectory ]; then
-        cp $braveBrowserBookmarks "$braveBackupDirectory/bookmarks_$currentDate.html"
-        if [ $? -eq 0 ]; then
-            print_color $BOLD_GREEN "Done!"
-        else
-            print_color $BOLD_RED "Could not copy file."
-        fi
+    cp $braveBrowserBookmarks "$braveBackupDirectory/bookmarks_$currentDate.html"
+    if [ $? -eq 0 ]; then
+        print_color $BOLD_GREEN "Done!"
     else
-        print_color $BOLD_YELLOW "Directory not found. Trying to create..."
-        mkdir -p $braveBackupDirectory
-        if [ $? -eq 0 ]; then
-            print_color $BOLD_GREEN "Directory created successfully."
-            cp $braveBrowserBookmarks "$braveBackupDirectory/bookmarks_$currentDate"
-            print_color $BOLD_GREEN "Done!"
-        else
-            print_color $BOLD_RED "Failed to create directory."
-        fi
+        print_color $BOLD_RED "Could not copy file."
     fi
 else
     print_color $BOLD_RED "Files not found! Skipping..."
@@ -64,23 +68,11 @@ if [ -f $zshConfig ]; then
     echo "."
     echo "."
     echo "."
-    if [ -d $zshBackupDirectory ]; then
-        cp $zshConfig "$zshBackupDirectory/.zshrc"
-        if [ $? -eq 0 ]; then
-            print_color $BOLD_GREEN "Done!"
-        else
-            print_color $BOLD_RED "Could not copy file."
-        fi
+    cp $zshConfig "$zshBackupDirectory/.zshrc"
+    if [ $? -eq 0 ]; then
+        print_color $BOLD_GREEN "Done!"
     else
-        print_color $BOLD_YELLOW "Directory not found. Trying to create..."
-        mkdir -p $zshBackupDirectory
-        if [ $? -eq 0 ]; then
-            print_color $BOLD_GREEN "Directory created successfully."
-            cp $zshConfig "$zshBackupDirectory/.zshrc"
-            print_color $BOLD_GREEN "Done!"
-        else
-            print_color $BOLD_RED "Failed to create directory."
-        fi
+        print_color $BOLD_RED "Could not copy file."
     fi
 else
     print_color $BOLD_RED "File not found! Skipping..."
