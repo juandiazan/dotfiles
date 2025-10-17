98babdc0aba232ce1bf45cf403330578ef41aea4
Juan Diaz <juanignaciodiazpuig998@gmail.com>
2025-10-17 20:27:37 -0300

    feat: included vs code settings and extensions

--- create-dotfiles-backup.sh
+++ create-dotfiles-backup.sh
@@ -7,15 +7,18 @@ BOLD_YELLOW='\033[1;33m'
 BOLD_PURPLE='\033[1;35m'
 NO_COLOR='\033[0m'
 currentDate=$(date +%d-%m-%Y_%H:%M:%S)
+getVsCodeExtensions=$(code --list-extensions)
 
 # dotfiles locations
 braveBrowserBookmarks="$HOME/.config/BraveSoftware/Brave-Browser/Default/Bookmarks"
 zshConfig="$HOME/.zshrc"
+vscodeSettings="$HOME/.config/Code/User/settings.json"
 
 # dotfiles destinations
 dotfilesBackupDir="$HOME/dotfiles"
 braveBackupDirectory="$dotfilesBackupDir/brave-browser"
 zshBackupDirectory="$dotfilesBackupDir/zsh-and-omz-config"
+vsCodeBackupDirectory="$dotfilesBackupDir/vs-code"
 
 print_color() {
 	echo -e $1$2$NO_COLOR
@@ -35,6 +38,7 @@ print_color $BOLD_PURPLE "-----------------------------"
 
 create_dir_if_not_exists $braveBackupDirectory
 create_dir_if_not_exists $zshBackupDirectory
+create_dir_if_not_exists $vsCodeBackupDirectory
 
 echo "Backing up files..."
 
@@ -44,9 +48,6 @@ echo "Backing up files..."
 echo "Trying to back up brave browser bookmarks..."
 if [ -f $braveBrowserBookmarks ]; then
 	echo "File found! Backing up..."
-	echo "."
-	echo "."
-	echo "."
 	cp $braveBrowserBookmarks "$braveBackupDirectory/bookmarks_$currentDate.html"
 	if [ $? -eq 0 ]; then
 		print_color $BOLD_GREEN "Done!"
@@ -63,9 +64,6 @@ fi
 echo "Trying to back up zsh config..."
 if [ -f $zshConfig ]; then
 	echo "File found! Backing up..."
-	echo "."
-	echo "."
-	echo "."
 	cp $zshConfig "$zshBackupDirectory/.zshrc"
 	if [ $? -eq 0 ]; then
 		print_color $BOLD_GREEN "Done!"
@@ -74,4 +72,28 @@ if [ -f $zshConfig ]; then
 	fi
 else
 	print_color $BOLD_RED "File not found! Skipping..."
+fi
+
+# -------------------------------------------------------
+# ----------------------- VS Code -----------------------
+# -------------------------------------------------------
+echo "Trying to back up VS Code settings..."
+if [ -f $vscodeSettings ]; then
+	echo "File found! Backing up..."
+	cp $vscodeSettings "$vsCodeBackupDirectory/settings.json"
+	if [ $? -eq 0 ]; then
+		print_color $BOLD_GREEN "Done!"
+	else
+		print_color $BOLD_RED "Could not copy file."
+	fi
+else
+	print_color $BOLD_RED "File not found! Skipping..."
+fi
+
+echo "Trying to back up VS Code installed extensions..."
+code --list-extensions >>"$vsCodeBackupDirectory/extensions.txt"
+if [ $? -eq 0 ]; then
+	print_color $BOLD_GREEN "Done!"
+else
+	print_color $BOLD_RED "Could save extensions."
 fi
