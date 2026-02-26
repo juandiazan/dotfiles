b9cd6325a8d5fe99dda118c4621bb14d8826a630
Juan Diaz <juanignaciodiazpuig998@gmail.com>
2026-02-26 13:13:53 +0100

    feat: support for more software

--- setup-scripts/setup.sh
+++ setup-scripts/setup.sh
@@ -8,13 +8,31 @@ source ./libs/utilities.sh
 source ./libs/detect-package-manager.sh
 source ./libs/install-packages.sh
 
-PROGRAMS=("librewolf" "spotify" "spicetify" "steam")
+PROGRAMS=(
+    "librewolf"
+    "discord"
+    "spotify"
+    "steam"
+    "localsend"
+    "obsidian"
+
+    "zsh"
+    "spicetify"
+    "ckb-next (corsair drivers)"
+    "solaar (logitech drivers)"
+)
 
 declare -A PACKAGES=(
     [librewolf]="librewolf-bin"
+    [discord]="discord"
     [spotify]="spotify"
-    [spicetify]="spicetify-cli"
     [steam]="steam"
+    [localsend]="localsend"
+    [obsidian]="obsidian"
+
+    [spicetify]="spicetify-cli"
+    [ckb-next (corsair drivers)]="ckb-next"
+    [solaar (logitech drivers)]="solaar"
 )
 declare -a SELECTED=()
 
@@ -24,14 +42,14 @@ declare -a SELECTED=()
 show_menu() {
     echo "Select programs to install:"
     for i in "${!PROGRAMS[@]}"; do
-        prog="${PROGRAMS[$i]}"
+        current_prog="${PROGRAMS[$i]}"
         # Check if already selected
-        if [[ " ${SELECTED[*]} " == *" $prog "* ]]; then
+        if [[ " ${SELECTED[*]} " == *" $current_prog "* ]]; then
             mark="[x]"
         else
             mark="[ ]"
         fi
-        printf "%2d) %s %s\n" $((i+1)) "$mark" "$prog"
+        printf "%2d) %s %s\n" $((i+1)) "$mark" "$current_prog"
     done
     echo "i) Install selected"
     echo "q) Quit"
@@ -46,11 +64,11 @@ select_software() {
 
         if [[ "$choice" =~ ^[0-9]+$ ]]; then
             index=$((choice-1))
-            prog="${PROGRAMS[$index]}"
-            if [[ " ${SELECTED[*]} " == *" $prog "* ]]; then
-                SELECTED=("${SELECTED[@]/$prog}") # remove from selected
+            current_prog="${PROGRAMS[$index]}"
+            if [[ " ${SELECTED[*]} " == *" $current_prog "* ]]; then
+                SELECTED=("${SELECTED[@]/$current_prog}") # remove from selected
             else
-                SELECTED+=("$prog")
+                SELECTED+=("$current_prog")
             fi
         elif [[ "$choice" == "i" ]]; then
             install_selected_software
