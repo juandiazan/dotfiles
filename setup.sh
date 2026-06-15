02370c4a0d6cb164f7e96991647c69fc1ec2b692
Juan Diaz <juanignaciodiazpuig998@gmail.com>
2026-06-15 17:11:38 -0300

    chore: backup

--- setup.sh
+++ setup.sh
@@ -9,31 +9,32 @@ source "$SETUP_SCRIPT_DIR/src/config/apply-configs.sh"
 source "$SETUP_SCRIPT_DIR/src/config/backup-configs.sh"
 
 print_color $BOLD_GREEN "Welcome to app and config setup!"
- 
+
 main_menu
 read option
 
-while [ $option -ne 0 ] ; do
+while [ $option -ne 0 ]; do
 
     case $option in
 
     1)
         clear
         select_software
-    ;; 
+        ;;
 
     2)
         clear
         select_configs
-    ;; 
+        ;;
 
     3)
         clear
         select_backups
-    ;;
+        ;;
 
     *)
         print_color $BOLD_RED "Invalid option."
+        ;;
     esac
 
     main_menu
